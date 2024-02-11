6fc36797676649911bce307cf190bb4835ae772b
Hleb Yakavinich <fosterlounch@gmail.com>
2024-02-11 17:10:09 +0300

    add cristalix...

--- fish/config.fish
+++ fish/config.fish
@@ -8,12 +8,15 @@ zoxide init fish | source
 export XDG_CONFIG_HOME="$(xdg-user-dir CONFIG)"
 export XDG_DATA_HOME="$(xdg-user-dir DATA)"
 export XDG_STATE_HOME="$(xdg-user-dir STATE)"
+export XDG_CACHE_HOME="$(xdg-user-dir CACHE)"
 
 export HISTFILE="$XDG_STATE_HOME/bash/history"
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
-export _JAVA_OPTIONS="$_JAVA_OPTIONS:-Djavafx.cachedir=$XDG_CACHE_HOME/openjfx"
-export _JAVA_OPTIONS="$_JAVA_OPTIONS:-Djava.util.prefs.userRoot=$XDG_CACHE_HOME/java"
+#export _JAVA_OPTIONS="-Djavafx.cachedir=$XDG_CACHE_HOME/openjfx"
+#export _JAVA_OPTIONS="$_JAVA_OPTIONS:-Djava.util.prefs.userRoot=$XDG_CACHE_HOME/java"
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME/pass"
+
+export PATH="$PATH":"$HOME/.local/bin"
